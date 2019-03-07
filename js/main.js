38b7ced939dc8e397e713fe44f1a074266a80456
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-03-07 15:33:05 -0300

    allows for different tints per state

--- js/main.js
+++ js/main.js
@@ -1,34 +1,42 @@
 var canvas;
 
 var state_list = [
-  { state: "AC", x: 143, y: 419, el: null, img: null },
-  { state: "AL", x: 931, y: 437, el: null, img: null },
-  { state: "AM", x: 301, y: 311, el: null, img: null },
-  { state: "AP", x: 565, y: 171, el: null, img: null },
-  { state: "BA", x: 801, y: 492, el: null, img: null },
-  { state: "CE", x: 845, y: 324, el: null, img: null },
-  { state: "DF", x: 657, y: 555, el: null, img: null },
-  { state: "ES", x: 822, y: 659, el: null, img: null },
-  { state: "GO", x: 606, y: 600, el: null, img: null },
-  { state: "MA", x: 717, y: 326, el: null, img: null },
-  { state: "MG", x: 733, y: 640, el: null, img: null },
-  { state: "MS", x: 500, y: 677, el: null, img: null },
-  { state: "MT", x: 473, y: 517, el: null, img: null },
-  { state: "PA", x: 545, y: 317, el: null, img: null },
-  { state: "PB", x: 934, y: 364, el: null, img: null },
-  { state: "PE", x: 949, y: 403, el: null, img: null },
-  { state: "PI", x: 770, y: 386, el: null, img: null },
-  { state: "PR", x: 577, y: 777, el: null, img: null },
-  { state: "RJ", x: 775, y: 726, el: null, img: null },
-  { state: "RN", x: 911, y: 336, el: null, img: null },
-  { state: "RO", x: 331, y: 466, el: null, img: null },
-  { state: "RR", x: 351, y: 163, el: null, img: null },
-  { state: "RS", x: 541, y: 893, el: null, img: null },
-  { state: "SC", x: 610, y: 842, el: null, img: null },
-  { state: "SE", x: 900, y: 464, el: null, img: null },
-  { state: "SP", x: 639, y: 723, el: null, img: null },
-  { state: "TO", x: 652, y: 455, el: null, img: null }
+  { state: "AC", x: 143, y: 419, el: null, img: null, region: "N" },
+  { state: "AL", x: 931, y: 437, el: null, img: null, region: "NE" },
+  { state: "AM", x: 301, y: 311, el: null, img: null, region: "N" },
+  { state: "AP", x: 565, y: 171, el: null, img: null, region: "N" },
+  { state: "BA", x: 801, y: 492, el: null, img: null, region: "NE" },
+  { state: "CE", x: 845, y: 324, el: null, img: null, region: "NE" },
+  { state: "DF", x: 657, y: 555, el: null, img: null, region: "CO" },
+  { state: "ES", x: 822, y: 659, el: null, img: null, region: "SE" },
+  { state: "GO", x: 606, y: 600, el: null, img: null, region: "CO" },
+  { state: "MA", x: 717, y: 326, el: null, img: null, region: "NE" },
+  { state: "MG", x: 733, y: 640, el: null, img: null, region: "SE" },
+  { state: "MS", x: 500, y: 677, el: null, img: null, region: "CO" },
+  { state: "MT", x: 473, y: 517, el: null, img: null, region: "CO" },
+  { state: "PA", x: 545, y: 317, el: null, img: null, region: "N" },
+  { state: "PB", x: 934, y: 364, el: null, img: null, region: "NE" },
+  { state: "PE", x: 949, y: 403, el: null, img: null, region: "NE" },
+  { state: "PI", x: 770, y: 386, el: null, img: null, region: "NE" },
+  { state: "PR", x: 577, y: 777, el: null, img: null, region: "S" },
+  { state: "RJ", x: 775, y: 726, el: null, img: null, region: "SE" },
+  { state: "RN", x: 911, y: 336, el: null, img: null, region: "NE" },
+  { state: "RO", x: 331, y: 466, el: null, img: null, region: "N" },
+  { state: "RR", x: 351, y: 163, el: null, img: null, region: "N" },
+  { state: "RS", x: 541, y: 893, el: null, img: null, region: "S" },
+  { state: "SC", x: 610, y: 842, el: null, img: null, region: "S" },
+  { state: "SE", x: 900, y: 464, el: null, img: null, region: "NE" },
+  { state: "SP", x: 639, y: 723, el: null, img: null, region: "SE" },
+  { state: "TO", x: 652, y: 455, el: null, img: null, region: "N" }
 ];
+
+let tints = {
+  N: [255, 0, 0],
+  NE: [255, 0, 255],
+  CO: [255, 255, 0],
+  SE: [0, 255, 0],
+  S: [0, 0, 255]
+};
 
 let myFont;
 let brazil;
@@ -65,8 +73,10 @@ function draw() {
       if (element.img && isNumeric(element.el.value)) {
         let min = 0.05;
         let range = 1.0 - min;
-        let opacity = (parseInt(element.el.value) / max) * range + min;
-        tint(255, opacity * 255);
+        let opacity =
+          (parseInt(element.el.value) / max[element.region]) * range + min;
+        t = tints[element.region];
+        tint([t[0], t[1], t[2], opacity * 255]);
         image(element.img, 0, 0);
       }
     }
@@ -88,11 +98,20 @@ function isNumeric(n) {
 }
 
 function findMax() {
-  let m = 0;
+  let m = {
+    N: 0,
+    NE: 0,
+    CO: 0,
+    SE: 0,
+    S: 0
+  };
   state_list.forEach(function(element) {
     if (element.el) {
-      if (isNumeric(element.el.value) && parseInt(element.el.value) > m) {
-        m = parseInt(element.el.value);
+      if (
+        isNumeric(element.el.value) &&
+        parseInt(element.el.value) > m[element.region]
+      ) {
+        m[element.region] = parseInt(element.el.value);
       }
     }
   });
